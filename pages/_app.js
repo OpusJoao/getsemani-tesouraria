80392d9bc3f85802cb070fefe5bc368856baccf2
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-01 11:37:17 -0300

    feat: add head to all plataform

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import styles from '../styles/Home.module.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+       <div className={styles.titleInstitution}>
+         <h1>GETSÊMANI</h1>
+         <Component {...pageProps} />
+      </div>
+    </div>
+    
+  )
 }
 
 export default MyApp

547d8a64af82817fe615b1e4ed3ce2bc256ff354
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-01 11:36:29 -0300

    feat: new component cardInfo

---
+++ pages/components/cardInfo/index.js
@@ -0,0 +1,24 @@
+import styles from '../../../styles/Home.module.css'
+
+export default function CardInfo({hasImg = true, title, text, img,footer}){
+  return(
+  <div className={styles.cardInfo}>
+    <div className={styles.cardInfoTitle}>
+      <span>{title}</span>
+    </div>
+    <div className={styles.cardInfoBody}>
+      <div className={styles.cardInfoBodyText}>
+        {text?.length > 0 && text.split(/\n/).map((val, index)=>(
+         <p key={val+index}>{val}</p>
+       ))}
+      </div>
+      {hasImg && <div className={styles.cardInfoBodyImg}>
+        <img src={img} width='96' height='96'/>
+      </div>}
+    </div>
+    <div className={styles.cardInfoFooter}>
+      <span>{footer}</span>
+    </div>
+  </div>
+  )
+}

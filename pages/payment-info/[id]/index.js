e2aa1ea5b8598b60ff4e6645d56f9004a1af5b57
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-01 16:28:53 -0300

    feat: ensure payment-info/[id] receive data from env file

--- pages/payment-info/[id]/index.js
+++ pages/payment-info/[id]/index.js
@@ -1,53 +1,50 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Button from '../../components/button';
 import Card from '../../components/card';
 import CardInfo from '../../components/cardInfo';
 
-export default function PaymentInfo(props){
-  const router = useRouter();
-  const {id} = router.query
+function PaymentInfo({currentPaymentInfo}){
   return(
     <div>
       <Card 
         hasButton={false} 
         icon={'/img/icons/camping.png'} 
         buttonText={'Pagar'} 
-        title={'[Jovens] - Acampaget'} 
-        linkTo={'/payment-info/1'}
+        title={currentPaymentInfo?.title}
         />
       <Card 
         hasButton={false} 
-        icon={'/img/icons/money.png'} 
+        icon={currentPaymentInfo?.icon} 
         buttonText={'Pagar'} 
-        title={'Tesoureiro: João Ferreira\n(15) 98829-9752'} 
-        linkTo={'/payment-info/1'}
+        title={`Tesoureiro: ${currentPaymentInfo?.treasurer}\n${currentPaymentInfo?.formatedPhone}`} 
         additonalInformation={
           <div style={{width:'100%'}}>
             <Button 
               text='Iniciar conversa no WhatsApp' 
               bgColor='#44B564' 
-              link={'https://wa.me/+5515988299752'}
+              link={`https://wa.me/${currentPaymentInfo?.phone}`}
               />
             <CardInfo 
               title={'PIX'} 
               img={'/img/icons/qrcode.png'} 
-              footer={'123123codigopix12313.oim.br1231231'} 
-              text={'Nome: João Vitor Antunes Ferreira\nValor: R$ 80,00\nBanco: Santander'}
+              footer={currentPaymentInfo?.pix?.code} 
+              text={`Nome: ${currentPaymentInfo?.pix?.name}\nValor: R$ ${currentPaymentInfo?.pix?.value}\nBanco: ${currentPaymentInfo?.pix?.bank}`}
               />
             <Button 
               text='Copiar código PIX' 
               ableToCopy={true} 
-              textToCopy={'123123codigopix12313.oim.br1231231'}
+              textToCopy={currentPaymentInfo?.pix?.code}
               />
             <CardInfo 
               title={'Dados Bancários'} 
               hasImg={false}
               footer={''} 
-              text={'Nome: João Vitor Antunes Ferreira\nCPF: 518.614.948-90\nNº Conta:\nAgência:\nTipo: Conta Corrente'}
+              text={`Nome: ${currentPaymentInfo?.ted.name}\nDOC: ${currentPaymentInfo?.ted.document}\nNº Conta:${currentPaymentInfo?.ted.accountNumber}\nAgência:${currentPaymentInfo?.ted.agency}\nTipo: ${currentPaymentInfo?.ted.typeAccount}`}
               />
             <Button 
               text='Enviar comprovante de depósito'
-              link={'https://wa.me/+5515988299752'}
+              link={`https://wa.me/${currentPaymentInfo?.phone}`}
               />
           </div>}
         />
@@ -55,3 +52,20 @@ export default function PaymentInfo(props){
     </div>
   )
 }
+
+export function getServerSideProps(ctx) {
+  const { params } = ctx;
+  const { id } = params;
+  const AllpaymentsInfo = process.env.PAYMENTS_INFO ? JSON.parse(process.env.PAYMENTS_INFO) : null
+  const currentPaymentInfo =  AllpaymentsInfo?.filter(val => val.id == id)[0] || null
+  return {
+    props: {
+      key: id,
+      id,
+      currentPaymentInfo
+    }, 
+  };
+}
+
+
+export default PaymentInfo

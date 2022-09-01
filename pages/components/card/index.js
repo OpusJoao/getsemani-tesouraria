7f454c6824431665923f31412b73521c19140907
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-01 11:36:09 -0300

    feat: new component card

---
+++ pages/components/card/index.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from '../../../styles/Home.module.css'
+
+export default function Card({title, buttonText, linkTo, icon, hasButton, additonalInformation}){
+  return(
+  <div className={styles.card}>
+    <div className={styles.cardIcon}>
+      <img src={icon} width="24"/>
+    </div>
+    <div className={styles.cardTitle}>
+       {title?.length > 0 && title.split(/\n/).map((val, index)=>(
+         <p key={val+index}>{val}</p>
+       ))}
+    </div>
+    {additonalInformation}
+    {hasButton !== false && <div className={styles.cardButton}>
+      <Link href={linkTo}>{buttonText}</Link>
+    </div>}
+  </div>
+  )
+}

ef294eeb611587d94f44f56aee1b24b6fa907891
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-01 16:28:27 -0300

    feat: ensure home receive data from env file

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Card from './components/card'
 
-export default function Home() {
+function Home({payments}) {
+  payments = payments ? JSON.parse(payments) : []
   return (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Card 
-       icon={'/img/icons/camping.png'} 
-       buttonText={'Pagar'} 
-       title={'[Jovens] - Acampaget'} 
-       linkTo={'/payment-info/1'}/>
+      {payments.map((payment) => {
+        return (
+        <Card 
+          key={payment.id}
+          icon={payment.img} 
+          buttonText={'Pagar'} 
+          title={payment.title} 
+          linkTo={`/payment-info/${payment.id}`}/>
+          )
+      })}
       <span style={{color: '#9F9F9F', marginTop: '3rem'}}>Estes são os únicos pagamentos</span>
     </div>
   )
 }
+
+Home.getInitialProps =  () => {
+  return {payments : process.env.PAYMENTS}
+}
+
+export default Home

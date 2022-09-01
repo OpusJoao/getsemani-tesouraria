559ea7ea95ec5bd94ea2768337c1df2a957378ed
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-01 12:09:27 -0300

    refactor: change tag img to Image from next

--- pages/components/card/index.js
+++ pages/components/card/index.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../../../styles/Home.module.css'
 
 export default function Card({title, buttonText, linkTo, icon, hasButton, additonalInformation}){
   return(
   <div className={styles.card}>
     <div className={styles.cardIcon}>
-      <img src={icon} width="24"/>
+      <Image src={icon} width='24' height='24'/>
     </div>
     <div className={styles.cardTitle}>
        {title?.length > 0 && title.split(/\n/).map((val, index)=>(
